962a962186ff12840edbda76e1da738cc8a6dd5d
snehab050-techie <snehab050@gmail.com>
2026-08-23 16:00:44 +0530

    Initial commit

---
+++ src/main/java/com/employee/EmployeeRestApis/EmployeeRestApisApplication.java
@@ -0,0 +1,13 @@
+package com.employee.EmployeeRestApis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRestApisApplication.class, args);
+	}
+
+}
