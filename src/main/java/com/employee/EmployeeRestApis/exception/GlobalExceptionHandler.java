32f97100891a2bdb945bebc3a74e090ec0d7d6b7
snehab050-techie <snehab050@gmail.com>
2026-08-27 21:17:41 +0530

    Committing changes to GlobalExceptionHandler to send more useful information about exception to the user

--- src/main/java/com/employee/EmployeeRestApis/exception/GlobalExceptionHandler.java
+++ src/main/java/com/employee/EmployeeRestApis/exception/GlobalExceptionHandler.java
@@ -5,13 +5,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmployeeNotFoundException.class)
-    public ResponseEntity<String> handleEmployeeNotFoundException(EmployeeNotFoundException employeeNotFoundException){
+    public ResponseEntity<ErrorResponse> handleEmployeeNotFoundException(EmployeeNotFoundException employeeNotFoundException){
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                404,
+                employeeNotFoundException.getMessage(),
+                LocalDateTime.now().toString()
+        );
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(employeeNotFoundException.getMessage());
+                .body(errorResponse);
     }
 }
+
+/*Now the response to client is:
+        {
+        "status": 404,
+        "message": "Employee not found with id: 100",
+        "timestamp": "2026-08-27T21:15:10.188724200"
+        }*/
