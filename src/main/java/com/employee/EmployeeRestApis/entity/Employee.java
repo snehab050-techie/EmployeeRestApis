274c893835ca3cbe45744c0107f70b36e32ff38f
snehab050-techie <snehab050@gmail.com>
2026-08-28 23:39:26 +0530

    Adding validation annotations for employee entity with proper messages

--- src/main/java/com/employee/EmployeeRestApis/entity/Employee.java
+++ src/main/java/com/employee/EmployeeRestApis/entity/Employee.java
@@ -1,10 +1,18 @@
 package com.employee.EmployeeRestApis.entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
 public class Employee {
 
     private int id;
+
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
+    @NotBlank(message = "Department cannot be blank")
     private String dept;
+
+    @Positive(message = "Salary must be greater than 0")
     private int salary;
 
     public int getId() {
