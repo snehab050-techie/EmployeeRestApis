a224b866ee6df133923d8184f4d2add83550e2c4
snehab050-techie <snehab050@gmail.com>
2026-09-03 10:55:31 +0530

    Changed database configuration to successfully connect our application to Mysql DB and excluded DataSourceAutoConfiguration property on main class

--- src/main/java/com/employee/EmployeeRestApis/EmployeeRestApisApplication.java
+++ src/main/java/com/employee/EmployeeRestApis/EmployeeRestApisApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//Commented above line - as its prevents our application from connecting to the database
+@SpringBootApplication
 public class EmployeeRestApisApplication {
 
 	public static void main(String[] args) {
